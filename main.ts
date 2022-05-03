adae5ed36f904820a1f66ebe8d7e2ca4b883db97
svi-berlin <svi-berlin@users.noreply.github.com>
2022-05-03 08:45:13 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,89 +4,96 @@ function spieleIntro () {
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
 basic.setLedColor(0x00ff00)
 spieleIntro()
-let frage = 0
+let fortschrittsanzeige = 0
+let quizfrage1 = 1
+let quizfrage2 = 4
+let erstefragebeantwortet = 0
+let nochkeinefragegestellt = 0
+let fragegestellt = 0
 basic.forever(function () {
-    if (frage == 0) {
+    if (fortschrittsanzeige == nochkeinefragegestellt) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 1
-            serialmp3.playMp3TrackFromFolder(1, 2, Mp3Repeat.No)
+            serialmp3.playMp3TrackFromFolder(quizfrage1, 2, Mp3Repeat.No)
+            fragegestellt = 1
+            fortschrittsanzeige = fragegestellt
         }
     }
-    if (frage == 1) {
+    if (fortschrittsanzeige == 1) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 2
+            fortschrittsanzeige = 2
             serialmp3.playMp3TrackFromFolder(2, 2, Mp3Repeat.No)
         } else {
             if (input.buttonIsPressed(Button.B)) {
-                frage = 2
+                fortschrittsanzeige = 2
+                erstefragebeantwortet = 2
                 serialmp3.playMp3TrackFromFolder(3, 2, Mp3Repeat.No)
             }
         }
     }
-    if (frage == 2) {
+    if (fortschrittsanzeige == erstefragebeantwortet) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 3
-            serialmp3.playMp3TrackFromFolder(4, 2, Mp3Repeat.No)
+            fortschrittsanzeige = 3
+            serialmp3.playMp3TrackFromFolder(quizfrage2, 2, Mp3Repeat.No)
         }
     }
-    if (frage == 3) {
+    if (fortschrittsanzeige == 3) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 4
+            fortschrittsanzeige = 4
             serialmp3.playMp3TrackFromFolder(5, 2, Mp3Repeat.No)
         } else {
             if (input.buttonIsPressed(Button.B)) {
-                frage = 4
+                fortschrittsanzeige = 4
                 serialmp3.playMp3TrackFromFolder(6, 2, Mp3Repeat.No)
             }
         }
     }
-    if (frage == 4) {
+    if (fortschrittsanzeige == 4) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 5
+            fortschrittsanzeige = 5
             serialmp3.playMp3TrackFromFolder(7, 2, Mp3Repeat.No)
         }
     }
-    if (frage == 5) {
+    if (fortschrittsanzeige == 5) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 6
+            fortschrittsanzeige = 6
             serialmp3.playMp3TrackFromFolder(8, 2, Mp3Repeat.No)
         } else {
             if (input.buttonIsPressed(Button.B)) {
-                frage = 6
+                fortschrittsanzeige = 6
                 serialmp3.playMp3TrackFromFolder(9, 2, Mp3Repeat.No)
             }
         }
     }
-    if (frage == 6) {
+    if (fortschrittsanzeige == 6) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 7
+            fortschrittsanzeige = 7
             serialmp3.playMp3TrackFromFolder(10, 2, Mp3Repeat.No)
         }
     }
-    if (frage == 7) {
+    if (fortschrittsanzeige == 7) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 8
+            fortschrittsanzeige = 8
             serialmp3.playMp3TrackFromFolder(12, 2, Mp3Repeat.No)
         } else {
             if (input.buttonIsPressed(Button.B)) {
-                frage = 8
+                fortschrittsanzeige = 8
                 serialmp3.playMp3TrackFromFolder(11, 2, Mp3Repeat.No)
             }
         }
     }
-    if (frage == 8) {
+    if (fortschrittsanzeige == 8) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 9
+            fortschrittsanzeige = 9
             serialmp3.playMp3TrackFromFolder(13, 2, Mp3Repeat.No)
         }
     }
-    if (frage == 9) {
+    if (fortschrittsanzeige == 9) {
         if (input.buttonIsPressed(Button.A)) {
-            frage = 0
+            fortschrittsanzeige = 0
             serialmp3.playMp3TrackFromFolder(14, 2, Mp3Repeat.No)
         } else {
             if (input.buttonIsPressed(Button.B)) {
-                frage = 0
+                fortschrittsanzeige = 0
                 serialmp3.playMp3TrackFromFolder(15, 2, Mp3Repeat.No)
             }
         }
