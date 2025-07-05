d8113cf64af100da1752715f6d3e3323cc7d1027
svi-berlin <svi-berlin@users.noreply.github.com>
2025-07-05 20:48:57 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 function spieleIntro () {
-    serialmp3.playMp3Folder(1, Mp3Repeat.No)
+    serialmp3.playMp3Track(1, 1)
 }
 function geschichteLesen () {
-    basic.setLedColor(0x00ff00)
+    basic.setLedColor(0x0000ff)
     basic.showNumber(lesen)
-    serialmp3.playMp3TrackFromFolder(lesen, 3, Mp3Repeat.No)
+    serialmp3.playMp3Track(lesen, 2)
     lesen += 1
     basic.setLedColor(0xff0000)
     if (lesen >= 9) {
         lesen = 1
     }
-    if (input.buttonIsPressed(Button.A)) {
-        lesen += -1
-    }
+}
+function gameOver () {
+    music.playMelody("E D G F B A C5 B ", 238)
 }
 let lesen = 0
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.SmallHeart)
 serialmp3.setMp3Volume(30)
-basic.setLedColor(0xffff00)
+basic.setLedColor(0x00ff00)
 lesen = 1
 let fortschrittsanzeige = 0
 let quizfrage1 = 1
@@ -29,16 +29,11 @@ let fragegestellt = 0
 spieleIntro()
 basic.forever(function () {
     if (fortschrittsanzeige == nochkeinefragegestellt) {
-        if (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             serialmp3.playMp3TrackFromFolder(1, 2, Mp3Repeat.No)
             fragegestellt = 1
             fortschrittsanzeige = fragegestellt
-        } else {
-            while (true) {
-                if (input.buttonIsPressed(Button.B)) {
-                    geschichteLesen()
-                }
-            }
+            geschichteLesen()
         }
     }
     if (fortschrittsanzeige == 1) {
@@ -55,7 +50,7 @@ basic.forever(function () {
         }
     }
     if (fortschrittsanzeige == 2) {
-        if (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             fortschrittsanzeige = 3
             basic.showNumber(4)
             serialmp3.playMp3TrackFromFolder(4, 2, Mp3Repeat.No)
@@ -75,7 +70,7 @@ basic.forever(function () {
         }
     }
     if (fortschrittsanzeige == 4) {
-        if (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             fortschrittsanzeige = 5
             basic.showNumber(7)
             serialmp3.playMp3TrackFromFolder(7, 2, Mp3Repeat.No)
@@ -95,7 +90,7 @@ basic.forever(function () {
         }
     }
     if (fortschrittsanzeige == 6) {
-        if (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             fortschrittsanzeige = 7
             basic.showNumber(10)
             serialmp3.playMp3TrackFromFolder(10, 2, Mp3Repeat.No)
@@ -115,7 +110,7 @@ basic.forever(function () {
         }
     }
     if (fortschrittsanzeige == 8) {
-        if (input.buttonIsPressed(Button.A)) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
             fortschrittsanzeige = 9
             basic.showNumber(13)
             serialmp3.playMp3TrackFromFolder(13, 2, Mp3Repeat.No)
@@ -132,6 +127,9 @@ basic.forever(function () {
                 basic.showNumber(15)
                 serialmp3.playMp3TrackFromFolder(15, 2, Mp3Repeat.No)
             }
+        }
+        if (fortschrittsanzeige == 0 && fragegestellt == 1) {
+            gameOver()
         }
     }
 })
